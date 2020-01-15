5522da3dac94de1043309e5e1149d09cd97aa327
Brandan Sayarath and Michelle Morin <brandan@>
2020-01-15 09:40:35 -0800

    add front-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,14 @@ var divide = function(number1, number2) {
 
 // front-end logic:
 
-var number1
-var number 2
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  })
+})
+
   
